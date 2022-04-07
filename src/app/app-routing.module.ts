7d2a37d503267a4ac0beb1db198d3e8eee93e804
Kredam <akitlosz2000@gmail.com>
2022-04-07 16:31:23 +0200

    closes #26 #42 Firebase Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {EntryComponent} from "./entry/entry.component";
+import {AuthGuard} from "./guards/auth.guard";
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileGuard } from './guards/profile.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent, canActivate:[ProfileGuard]},
+  {path: 'entry', component: EntryComponent, canActivate:[AuthGuard]},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
